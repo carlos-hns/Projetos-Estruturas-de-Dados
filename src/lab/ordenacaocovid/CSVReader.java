5e039ff549ee835047daa4cf58a30d837631b5bd
Carlos Henrique <carloshenriquend.santos@gmail.com>
2021-04-07 17:46:49 -0300

    Primeira versão das classes CVSReader e CovidData adcionadas

---
+++ src/lab/ordenacaocovid/CSVReader.java
@@ -0,0 +1,33 @@
+package lab.ordenacaocovid;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVReader {
+    static CovidData[] getData(String path) {
+        List<CovidData> data = new ArrayList<CovidData>();
+        try {
+            FileReader fileReader = new FileReader(path);
+            BufferedReader reader = new BufferedReader(fileReader);
+
+            String linha = "";
+            boolean isFirstLine = true;
+            while((linha = reader.readLine()) != null) {
+                if (isFirstLine) {
+                    isFirstLine = false;
+                    continue;
+                }
+                data.add(new CovidData(linha));
+            }
+        } catch (FileNotFoundException error) {
+            System.err.println("Não foi possível encontrar o arquivo.");
+        } catch (IOException error) {
+            System.err.println("Erro ao ler linha.");
+        }
+        return data.toArray(new CovidData[0]);
+    }
+}
---
+++ src/lab/ordenacaocovid/CovidData.java
@@ -0,0 +1,95 @@
+package lab.ordenacaocovid;
+
+public class CovidData {
+
+    private String data;
+    private String estado;
+    private String cidade;
+    private String tipoDoLugar;
+    private int casosConfirmados;
+    private int mortes;
+    private boolean dadoAtualizado;
+    private int populacaoEstimada;
+    private int ibgeCode;
+    private double confirmados100MilHabitantes;
+    private double taxaDeMorte;
+
+    private static final int dataIndex = 0;
+    private static final int estadoIndex = 1;
+    private static final int cidadeIndex = 2;
+    private static final int tipoDoLugarIndex = 3;
+    private static final int casosConfirmadorIndex = 4;
+    private static final int mortesIndex = 5;
+    private static final int dadoAtualizadoIndex = 6;
+    private static final int populacaoEstimadaIndex = 7;
+    private static final int ibgeCodeIndex = 8;
+    private static final int confirmados100MilHabitantesIndex = 9;
+    private static final int taxaDeMorteIndex = 10;
+
+    public CovidData(String CVSLine) {
+        this.setarAtributos(CVSLine);
+    }
+
+    private void setarAtributos(String CSVLine) {
+        String[] colunas = separarColunas(CSVLine);
+        this.data = colunas[dataIndex];
+        this.estado = colunas[estadoIndex];
+        this.cidade = colunas[cidadeIndex];
+        this.tipoDoLugar = colunas[tipoDoLugarIndex];
+        this.casosConfirmados = !colunas[casosConfirmadorIndex].equals("") ? Integer.parseInt(colunas[casosConfirmadorIndex]) : 0;
+        this.mortes = !colunas[mortesIndex].equals("") ? Integer.parseInt(colunas[mortesIndex]): 0;
+        this.dadoAtualizado = colunas[dadoAtualizadoIndex].equals("True");
+        this.populacaoEstimada = !colunas[populacaoEstimadaIndex].equals("") ? Integer.parseInt(colunas[populacaoEstimadaIndex]) : 0;
+        this.ibgeCode = !colunas[ibgeCodeIndex].equals("") ? Integer.parseInt(colunas[ibgeCodeIndex]) : 0;
+        this.confirmados100MilHabitantes = !colunas[confirmados100MilHabitantesIndex].equals("") ? Double.parseDouble(colunas[confirmados100MilHabitantesIndex]) : 0;
+        this.taxaDeMorte = !colunas[taxaDeMorteIndex].equals("") ? Double.parseDouble(colunas[taxaDeMorteIndex]) : 0;
+    }
+
+    private String[] separarColunas(String CSVLine) {
+        return CSVLine.split(",");
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getTipoDoLugar() {
+        return tipoDoLugar;
+    }
+
+    public int getCasosConfirmados() {
+        return casosConfirmados;
+    }
+
+    public int getMortes() {
+        return mortes;
+    }
+
+    public boolean isDadoAtualizado() {
+        return dadoAtualizado;
+    }
+
+    public int getPopulacaoEstimada() {
+        return populacaoEstimada;
+    }
+
+    public int getIbgeCode() {
+        return ibgeCode;
+    }
+
+    public double getConfirmados100MilHabitantes() {
+        return confirmados100MilHabitantes;
+    }
+
+    public double getTaxaDeMorte() {
+        return taxaDeMorte;
+    }
+}
