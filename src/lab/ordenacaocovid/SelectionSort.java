8616e7f172ebba78fad8c8cd1813ebd745d6c84c
Carlos Henrique <carloshenriquend.santos@gmail.com>
2021-04-08 12:19:01 -0300

    SelectionSort implementado com ordenação para obitos, cidades e casos.

--- src/lab/ordenacaocovid/SelectionSort.java
+++ src/lab/ordenacaocovid/SelectionSort.java
@@ -1,4 +1,54 @@
 package lab.ordenacaocovid;
 
+import java.util.Arrays;
+
 public class SelectionSort {
+    public static void ordenarPorObitos(CovidData[] vector) {
+        int vectorSize = vector.length;
+        for (int i=0; i < vectorSize; i++) {
+            CovidData currentCovidDataToOrder = vector[i];
+            int lowestCovidDataIndex = i;
+            int nextCovidDataOfSearch = i + 1;
+            for (int j = nextCovidDataOfSearch; j < vectorSize; j++) {
+                if(vector[j].getMortes() < vector[nextCovidDataOfSearch].getMortes()) {
+                    lowestCovidDataIndex = j;
+                }
+            }
+            vector[i] = vector[lowestCovidDataIndex];
+            vector[lowestCovidDataIndex] = currentCovidDataToOrder;
+        }
+    }
+
+    public static void ordenarPorCasos(CovidData[] vector) {
+        int vectorSize = vector.length;
+        for (int i=0; i < vectorSize; i++) {
+            CovidData currentCovidDataToOrder = vector[i];
+            int lowestCovidDataIndex = i;
+            int nextCovidDataOfSearch = i + 1;
+            for (int j = nextCovidDataOfSearch; j < vectorSize; j++) {
+                if(vector[j].getCasosConfirmados() < vector[nextCovidDataOfSearch].getCasosConfirmados()) {
+                    lowestCovidDataIndex = j;
+                }
+            }
+            vector[i] = vector[lowestCovidDataIndex];
+            vector[lowestCovidDataIndex] = currentCovidDataToOrder;
+        }
+    }
+
+    public static void ordenarPorNomeDasCidades(CovidData[] vector) {
+        int vectorSize = vector.length;
+        for (int i=0; i < vectorSize; i++) {
+            CovidData currentCovidDataToOrder = vector[i];
+            int lowestCovidDataIndex = i;
+            int nextCovidDataOfSearch = i + 1;
+            for (int j = nextCovidDataOfSearch; j < vectorSize; j++) {
+                if((vector[j].getCidade().compareTo(vector[nextCovidDataOfSearch].getCidade())) < 0) {
+                    lowestCovidDataIndex = j;
+                }
+            }
+            vector[i] = vector[lowestCovidDataIndex];
+            vector[lowestCovidDataIndex] = currentCovidDataToOrder;
+        }
+    }
+
 }
