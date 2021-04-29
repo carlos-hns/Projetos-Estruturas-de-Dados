c03306f28565fedd44f5d5ffe701738005b0a4a1
Carlos Henrique <carloshenriquend.santos@gmail.com>
2021-04-29 09:55:41 -0300

    Adcionado strings de caminhos na classe strings, CSVCleaner modificado para não aceitar estados

--- src/lab/ordenacaocovid/csv/CSVCleaner.java
+++ src/lab/ordenacaocovid/csv/CSVCleaner.java
@@ -8,7 +8,7 @@ public class CSVCleaner {
          Vector<CovidData> filteredData = new Vector<CovidData>(dados.size());
          int indexOfDataFiltered = 0;
          for (int i = 0; i < dados.size(); i++) {
-             if(dados.findWithIndex(i).isDadoAtualizado()) {
+             if(dados.findWithIndex(i).isDadoAtualizado() && dados.findWithIndex(i).getTipoDoLugar().equals("city")) {
                  filteredData.add(dados.findWithIndex(i), indexOfDataFiltered);
                  indexOfDataFiltered++;
              }
--- src/lab/ordenacaocovid/utils/Strings.java
+++ src/lab/ordenacaocovid/utils/Strings.java
@@ -3,4 +3,17 @@ package lab.ordenacaocovid.utils;
 public class Strings {
     public static final String CABECALHO =
             "date,state,city,place_type,confirmed,deaths,order_for_place,is_last,estimated_population_2019,estimated_population,city_ibge_code,confirmed_per_100k_inhabitants,death_rate\n";
+
+    public static final String PATH_OBITOS_MELHOR = "{0}/caso-limpo-obitos-melhor.csv";
+    public static final String PATH_OBITOS_MEDIO = "{0}/caso-limpo-obitos-medio.csv";
+    public static final String PATH_OBITOS_PIOR = "{0}/caso-limpo-obitos-pior.csv";
+
+    public static final String PATH_CONFIRMADOS_MELHOR = "{0}/caso-limpo-confirmados-melhor.csv";
+    public static final String PATH_CONFIRMADOS_MEDIO = "{0}/caso-limpo-confirmados-medio.csv";
+    public static final String PATH_CONFIRMADOS_PIOR = "{0}/caso-limpo-confirmados-pior.csv";
+
+    public static final String PATH_CIDADES_MELHOR = "{0}/caso-limpo-cidades-melhor.csv";
+    public static final String PATH_CIDADES_MEDIO = "{0}/caso-limpo-cidades-medio.csv";
+    public static final String PATH_CIDADES_PIOR = "{0}/caso-limpo-cidades-pior.csv";
+
 }
